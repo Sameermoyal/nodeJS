cb40d66699fa46a82ee8b6f50e70d63f694960db
Sameermoyal <sameerab827@gmail.com>
2023-10-25 14:26:07 +0530

    demo chat app

--- helloworld.js
+++ helloworld.js
@@ -1,2 +1,19 @@
-console.log('hello world')
+console.log('a');
+
+console.log('b');
+(async function  (){
+   await new Promise(resolve=>setTimeout(()=>{
+    console.log('c');
+    resolve();
+
+   },3000))
+
+   await new Promise(resolve=>setTimeout(()=>{
+    console.log('d');
+    resolve();
+   },0));
+   console.log('e');
+})()
+
+
 
